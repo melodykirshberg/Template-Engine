e488ba3adb5e12c883cb85ffe541eb2847b46ed3
verlitas <55808608+verlitas@users.noreply.github.com>
2019-11-10 19:30:37 -0700

    updated html

--- index.js
+++ index.js
@@ -10,17 +10,81 @@ const Engineer = require("./lib/constructors/Engineer");
 const Intern = require("./lib/constructors/Intern");
 
 // Global Variables
-const teamMembers = [
-  new Manager("Brent", 1, "brent@trilogyed.com", 200),
-  new Engineer("Tucker", 2, "tbeauchamp@2u.com", "tuckerbeauchamp"),
-  new Intern("Becky", 3, "becky@becky.com", "UofA"),
-  new Intern("Shelly", 4, "shelly@shelly.com", "UofA")
+const teamMembers = [];
+// questions to ask for manager position
+const managerQuestions = [
+  {
+      type: "input",
+      name: "name",
+      message: "What's the manager's name?",
+  },
+  {
+      type: "input",
+      name: "id",
+      message: "What's the manager's ID number?",
+  },
+  {
+      type: "input",
+      name: "email",
+      message: "What's the manager's email?",
+  },
+  {
+      type: "input",
+      name: "officeNumber",
+      message: "What's the manager's office number?",
+  }
+];
+// questions to ask for engineer position
+const engineerQuestions = [
+  {
+      type: "input",
+      name: "name",
+      message: "What's the engineers's name?",
+  },
+  {
+      type: "input",
+      name: "id",
+      message: "What's the engineers's ID number?",
+  },
+  {
+      type: "input",
+      name: "email",
+      message: "What's the engineers's email?",
+  },
+  {
+      type: "input",
+      name: "gitty",
+      message: "What's the engineers's GitHub username?",
+  }
+];
+// questions to ask for intern position
+const internQuestions = [
+  {
+      type: "input",
+      name: "name",
+      message: "What's the intern's name?",
+  },
+  {
+      type: "input",
+      name: "id",
+      message: "What's the intern's ID number?",
+  },
+  {
+      type: "input",
+      name: "email",
+      message: "What's the intern's email?",
+  },
+  {
+      type: "input",
+      name: "school",
+      message: "What's the intern's school?",
+  }
 ];
 
 
-
-
 async function init() {
+
+  
   render(teamMembers);
 }
 
